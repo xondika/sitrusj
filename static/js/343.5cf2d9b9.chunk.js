e6952adeab0d6c868a9ff4a48824c27397e95589
xondika <patrick.ondika@gmail.com>
2024-07-09 23:28:52 +0200

    Updates

---
+++ static/js/343.5cf2d9b9.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[343],{4343:(p,a,e)=>{e.r(a),e.d(a,{QuickJSModuleCallbacks:()=>u.q,QuickJSWASMModule:()=>u.t,applyBaseRuntimeOptions:()=>u.r,applyModuleEvalRuntimeOptions:()=>u.s});var u=e(1416)}}]);
